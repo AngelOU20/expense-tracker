4aa7932010d7eee0dd4c6c184cdfae3bc5f6a51a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-04 15:18:17 -0500

    refactor: update TransactionList component

--- src/components/transactions/TransactionList.jsx
+++ src/components/transactions/TransactionList.jsx
@@ -7,16 +7,26 @@ export const TransactionList = () => {
   const { transactions, deleteTransaction } = useGlobalState();
   return (
     <>
-      <h3 className="text-slate-300 text-xl font-bold block my-2">History</h3>
-      <ul>
-        {transactions.map((transaction) => (
-          <TransactionItem
-            key={transaction.id}
-            {...transaction}
-            deleteTransaction={deleteTransaction}
-          />
-        ))}
-      </ul>
+      {transactions.length > 0 ? (
+        <>
+          <h3 className="text-slate-300 text-xl font-bold block my-2">
+            History
+          </h3>
+          <ul>
+            {transactions.map((transaction) => (
+              <TransactionItem
+                key={transaction.id}
+                {...transaction}
+                deleteTransaction={deleteTransaction}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h2 className="text-slate-300 text-3xl font-extrabold my-2 flex justify-center items-center h-full">
+          No history
+        </h2>
+      )}
     </>
   );
 };
