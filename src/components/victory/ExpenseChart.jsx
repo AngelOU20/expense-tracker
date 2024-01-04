f5a8449b12add363094fa229cf90434ef3880a77
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-04 15:17:54 -0500

    refactor: add useGlobalState in ExpenseChart

--- src/components/victory/ExpenseChart.jsx
+++ src/components/victory/ExpenseChart.jsx
@@ -1,32 +1,39 @@
-// import PropTypes from "prop-types";
 import { VictoryPie, VictoryLabel } from "victory";
 import { useTransactionsTotal } from "../../hooks/useTransactionsTotal";
+import { useGlobalState } from "../../hooks/useGlobalState";
 
 export const ExpenseChart = () => {
+  const { transactions } = useGlobalState();
   const { income, expense } = useTransactionsTotal();
 
-  const expensePercentage = Math.round((expense / income) * 100);
-  const icomePercentage = 100 - expensePercentage;
+  const initialIncome = income || 0;
+  const initialExpense = expense || 0;
 
-  console.log({ icomePercentage, expensePercentage });
+  const expensePercentage =
+    initialIncome !== 0
+      ? Math.round((initialExpense / initialIncome) * 100)
+      : Math.round((initialExpense / initialExpense) * 100);
+  const icomePercentage = 100 - expensePercentage;
 
   return (
     <div className="w-full h-auto">
-      <VictoryPie
-        colorScale={["#b11e24", "#0fffa3"]}
-        animate={{ duration: 200 }}
-        data={[
-          { x: "Expenses", y: expensePercentage },
-          { x: "Incomes", y: icomePercentage },
-        ]}
-        padAngle={2}
-        cornerRadius={5}
-        innerRadius={100}
-        labels={({ datum }) => `${datum.y}%`}
-        labelComponent={<VictoryLabel angle={45} style={{ fill: "white" }} />}
-      />
+      {transactions.length > 0 ? (
+        <VictoryPie
+          colorScale={["#b11e24", "#0fffa3"]}
+          animate={{ duration: 400 }}
+          data={[
+            { x: "Expenses", y: expensePercentage },
+            { x: "Incomes", y: icomePercentage },
+          ]}
+          padAngle={2}
+          cornerRadius={5}
+          innerRadius={100}
+          labels={({ datum }) => `${datum.y}%`}
+          labelComponent={<VictoryLabel angle={10} style={{ fill: "white" }} />}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
-
-ExpenseChart.propTypes = {};
